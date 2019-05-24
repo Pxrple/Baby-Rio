fc8b992c2e6041fafabaa1c256437e999447692b
Oribuin <38044508+Oribuin@users.noreply.github.com>
2019-05-24 15:35:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+const { Client, RichEmbed } = require('discord.js');
+
+client.on('ready', () => {
+    console.log(`BabyRio Loaded! Playing In ${client.guilds.size} Servers`)
+});
+const activities_list = [
+    "with BabyOri", 
+    `in ${client.guilds.size} Servers!`,
+    "with Discord", 
+    "on my phone!"
+    ];
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+        client.user.setActivity(activities_list[index]);
+    }, 20000);
+});
+client.on('message', message => {
+    if (message.content === 'Rio') {
+        message.channel.send(`BabyRio is working in ${client.guilds.size} servers!`)
+    }   
+});
+client.login('NTgxMjAzOTcwMjAzMTg5MjY5.XOb_sQ.hPUhTLJYydkmiFeKZn8FPXuVT8c')
